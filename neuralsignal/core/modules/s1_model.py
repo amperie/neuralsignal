2c6027a761fc73fff983a8b23f7bf073f5558e1b
amperie <amperie@yahoo.com>
2024-05-06 08:18:41 -0700

    jj

---
+++ neuralsignal/core/modules/s1_model.py
@@ -0,0 +1,22 @@
+import logging
+import mlflow
+import os
+import pickle
+from neuralsignal.core.modules.utils import string_to_filename
+from neuralsignal.core.modules.neuralsignal_config import sdk_config
+
+logging.basicConfig(level=sdk_config.logging_level())
+
+
+class S1Model:
+    """
+    Simple class to just wrap a model so we can include metadata
+    about it an define interfaces for using it
+    """
+    def __init__(self, config: dict) -> None:
+        self.config = config
+        self.name = config["name"]
+        self.model_id = config["model_id"]
+        self.model = config['model']
+
+    
