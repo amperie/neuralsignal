2d1e29b72cd29d6b62c85649db26b90fd0831b23
amperie <amperie@yahoo.com>
2024-08-28 11:09:19 -0700

    cache

--- neuralsignal/backend/mongo_backend.py
+++ neuralsignal/backend/mongo_backend.py
@@ -109,7 +109,8 @@ class MongoBackend:
     def add_to_cache(self, scan: dict):
         if self.scan_cache_size > 0:
             id = str(scan["_id"])
-            scan.original_device = scan['outputs'].values()[0].device
+            scan.original_device =\
+                next(iter(scan['outputs'].values())).device
             scan = move_scan_to_device(scan, "cpu")
             if len(self.scan_cache.keys()) < self.scan_cache_size:
                 # Still have room in the cache so insert
