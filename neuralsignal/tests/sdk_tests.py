9f55a51043f22240f1b82fdaf2be8c3e8f2ce546
amperie <amperie@yahoo.com>
2024-08-29 21:09:51 -0700

    cache mvp

--- neuralsignal/tests/sdk_tests.py
+++ neuralsignal/tests/sdk_tests.py
@@ -208,7 +208,14 @@ def test_feature_processor():
         import FeatureSetTrueFalseDiff
 
     scan = load_scan("66caa52bd6b651131619e073", "hallucination")
+    scan = load_scan("66caa52ed6b651131619e307", "hallucination")
+    scan = load_scan("66caa533d6b651131619e7dd", "hallucination")
+    #scan = load_scan("66caa539d6b651131619ecb7", "hallucination")
+    #scan = load_scan("66caa53dd6b651131619f04f", "hallucination")
+
     scan = load_scan("66caa52bd6b651131619e073", "hallucination")
+    scan = load_scan("66caa52ed6b651131619e307", "hallucination")
+    scan = load_scan("66caa533d6b651131619e7dd", "hallucination")
     """zones_cfg = {
         "name": "zones",
         "target_zone_size": {"default": 256, ".o": 512, ".q": 1024},
