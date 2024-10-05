45e61d259b7ee7210cac1589f9fbcc2ca0db8966
amperie <amperie@yahoo.com>
2024-10-05 09:55:18 -0700

    dev_map in config

--- neuralsignal/core/modules/feature_sets/feature_set_t_f_diff.py
+++ neuralsignal/core/modules/feature_sets/feature_set_t_f_diff.py
@@ -30,8 +30,10 @@ class FeatureSetTrueFalseDiff(FeatureSetBase):
         """
         super().__init__(config)
 
-        if torch.cuda.is_available():
-            dev_map = "cuda:1"
+        if "dev_map" in config:
+            dev_map = config["dev_map"]
+        elif torch.cuda.is_available():
+            dev_map = "cuda:0"
         else:
             dev_map = "cpu"
         self.dev_map = dev_map
