bafe22948d22b69673bbb52c8f86d394bdda9fdb
amperie <amperie@yahoo.com>
2024-10-18 21:20:33 -0700

    remove

--- neuralsignal/sdk/neuralsignal.py
+++ neuralsignal/sdk/neuralsignal.py
@@ -32,7 +32,7 @@ class DetectionResults:
 
     def __str__(self):
         retVal = f"DetectionResults: {self.input} - {self.output}"\
-                f"- {self.ground_truth} - {self.metadata} - {self.detections["quora_duplicates"]}"
+                f"- {self.ground_truth} - {self.metadata} - {self.detections['quora_duplicates']}"
         return retVal
 
 
