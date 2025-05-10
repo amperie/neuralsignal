4ba225de65e7426fcc4bfe20789e8e38d042d1f6
amperie <pablo.i.salanova@gmail.com>
2025-05-10 15:01:35 -0700

    Refactor scan saving logic in SDK class to handle scan_delta type separately, ensuring proper backend saving for both delta and regular scans.

--- neuralsignal/sdk/neuralsignal.py
+++ neuralsignal/sdk/neuralsignal.py
@@ -298,11 +298,13 @@ class SDK:
 
         # Save to the backend if required
         if self.save_scans:
-            # for gi in gis:
-            #    self.backend.save_scan(gi)
-            self.backend.save_scan(
-                subtract_scans(gis[0], gis[1])
-            )
+            if d.type == "scan_delta":
+                self.backend.save_scan(
+                    subtract_scans(gis[0], gis[1])
+                )
+            else:
+                for gi in gis:
+                    self.backend.save_scan(gi)
 
         return retVal
 
