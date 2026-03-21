0958351fb19b7799237fbc1ac53ec214d31f3c14
amperie <pablo.i.salanova@gmail.com>
2026-03-21 08:50:50 -0700

    configs

--- neuralsignal/automation/dataset_automation.py
+++ neuralsignal/automation/dataset_automation.py
@@ -1,3 +1,4 @@
+import sys
 import logging
 from neuralsignal.automation.dataset_automation_core\
     import run_automation
@@ -11,6 +12,7 @@ logging.basicConfig(level=sdk_config.logging_level())
 # CONFIG AND SETUP                                       #
 ##########################################################
 
-config = get_config()
+cfg_path = sys.argv[1] if len(sys.argv) > 1 else None
+config = get_config(cfg_path)
 run_automation(config)
 logging.info("Automation complete")
